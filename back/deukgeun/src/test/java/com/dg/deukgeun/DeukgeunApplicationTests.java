fbfd8c6a8ef9b3da42605b4b3d42d4ad4e09bdbd
jngwk <jngwk.ch@gmail.com>
2024-05-22 01:05:48 +0900

    initial commit

---
+++ back/deukgeun/src/test/java/com/dg/deukgeun/DeukgeunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dg.deukgeun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeukgeunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
