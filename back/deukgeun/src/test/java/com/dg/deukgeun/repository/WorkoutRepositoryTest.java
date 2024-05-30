63c8021459239527230ef23e2de9485a6b1e8abf
YoungWoo0 <121625793+YoungWoo0@users.noreply.github.com>
2024-05-30 14:10:13 +0900

    test파일 추가

--- back/deukgeun/src/test/java/com/dg/deukgeun/repository/WorkoutRepositoryTest.java
+++ back/deukgeun/src/test/java/com/dg/deukgeun/repository/WorkoutRepositoryTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import com.dg.deukgeun.Entity.PtSession;
@@ -18,9 +19,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
 import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.ANY)
-@Log4j2
 public class WorkoutRepositoryTest {
 
     @Autowired
@@ -36,7 +37,10 @@ public class WorkoutRepositoryTest {
     private PtSession ptSession;
 
     @BeforeEach
-    void setUp() {
+    @Commit
+    @Transactional
+    @Test
+    public void setUp() {
         workoutSession = new WorkoutSession();
         workoutSession.setDate(LocalDate.now());
         workoutSession.setContent("Test Content");
@@ -56,35 +60,36 @@ public class WorkoutRepositoryTest {
     }
 
     @Test
-    @Transactional
-    void testCreateAndFindWorkout() throws Exception {
+    public void testCreateAndFindWorkout() throws Exception {
+    // 새로운 Workout 객체 생성
         Workout workout = new Workout();
         workout.setWorkoutSession(workoutSession);
         workout.setWorkoutName("Bench Press");
         workout.setWorkoutSet(3);
         workout.setWorkoutRep(12);
         workout.setWeight(75.0);
-        
+    
+    // Workout 객체 저장
         workoutRepository.save(workout);
-        
+    
         log.info("Workout saved: {}", workout);
     
+    // 저장된 Workout 객체를 읽어옴
         List<Workout> workouts = workoutRepository.findAll();
-        
+    
         log.info("Found workouts: {}", workouts);
+    
+    // 저장된 Workout 객체의 속성 값 로깅
+        for (Workout savedWorkout : workouts) {
+            log.info("Workout: ID={}, Name={}, Set={}, Rep={}, Weight={}",
+                 savedWorkout.getWorkoutId(), savedWorkout.getWorkoutName(),
+                 savedWorkout.getWorkoutSet(), savedWorkout.getWorkoutRep(),
+                 savedWorkout.getWeight());
+        }
     
         assertEquals(1, workouts.size());
         assertEquals("Bench Press", workouts.get(0).getWorkoutName());
     
-        // 객체를 JSON으로 변환하여 출력
-        String workoutJson = new ObjectMapper().writeValueAsString(workouts.get(0));
-        log.info("Workout JSON: {}", workoutJson);
-        
-        //workoutSession 정보 json 변혼 출력
-        String workoutSessionJson = new ObjectMapper().writeValueAsString(workouts.get(0).getWorkoutSession());
-        log.info("WorkoutSession JSON: {}", workoutSessionJson);
-
-
         log.info("-------------------success");
     }
 }
