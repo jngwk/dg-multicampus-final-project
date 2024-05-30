51377b53e851910bf547db79186c800354c3e350
gachudon <doneson7@gmail.com>
2024-05-30 13:47:17 +0900

    feat: The following class was added/modified to allow read access to the workout table.
    WorkoutService.java
    WorkoutController.java

--- back/deukgeun/src/main/java/com/dg/deukgeun/service/WorkoutService.java
+++ back/deukgeun/src/main/java/com/dg/deukgeun/service/WorkoutService.java
@@ -1,5 +1,7 @@
 package com.dg.deukgeun.service;
 
+import java.util.Optional;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
@@ -19,10 +21,28 @@ public class WorkoutService {
     private final ModelMapper modelMapper;
     private final WorkoutRepository workoutRepository;
 
-    public Integer register(WorkoutDTO workoutDTO){
+    public Integer register(WorkoutDTO workoutDTO){ //workout 정보 입력
         log.info("--------------------");
         Workout workout = modelMapper.map(workoutDTO,Workout.class);
         Workout savedWorkout = workoutRepository.save(workout);
         return savedWorkout.getWorkoutId();
+    }
+    public WorkoutDTO get(Integer workoutId){ //workoutId를 기준으로 read
+        Optional<Workout> result = workoutRepository.findById(workoutId);
+        Workout workout = result.orElseThrow();
+        WorkoutDTO dto = modelMapper.map(workout,WorkoutDTO.class);
+        return dto;
+    }
+
+    public void modify(WorkoutDTO workoutDTO){ //workoutId를 기준으로 update
+        Optional<Workout> result = workoutRepository.findById(workoutDTO.getWorkoutId());
+        Workout workout = result.orElseThrow();
+        workout.setWeight(workoutDTO.getWeight());
+        workout.setWorkoutName(workoutDTO.getWorkoutName());
+        workout.setWorkoutRep(workoutDTO.getWorkoutRep());
+        workout.setWorkoutSet(workoutDTO.getWorkoutSet());
+    }
+    public void remove(Integer workoutId){
+        workoutRepository.deleteById(workoutId);
     }
 }
