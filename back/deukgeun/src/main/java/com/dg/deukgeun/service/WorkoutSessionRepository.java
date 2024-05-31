c9e72a38a0ebee1f0c2d384085bed393acec4571
YoungWoo0 <121625793+YoungWoo0@users.noreply.github.com>
2024-05-31 09:20:46 +0900

    승돈님 일반회원  파일 인계중

---
+++ back/deukgeun/src/main/java/com/dg/deukgeun/service/WorkoutSessionRepository.java
@@ -0,0 +1,9 @@
+package com.dg.deukgeun.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dg.deukgeun.domain.WorkoutSession;
+
+public interface WorkoutSessionRepository extends JpaRepository<WorkoutSession,Integer>{
+    
+}
