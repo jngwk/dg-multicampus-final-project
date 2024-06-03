64ff197fb61949a25804bd472271e201c624ace5
jngwk <jngwk.ch@gmail.com>
2024-06-04 03:22:38 +0900

    timestamp 자동 생성 추가

--- back/deukgeun/src/main/java/com/dg/deukgeun/Entity/ChatMessage.java
+++ back/deukgeun/src/main/java/com/dg/deukgeun/Entity/ChatMessage.java
@@ -9,6 +9,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,27 +19,37 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "chat_message")
-@Builder
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class ChatMessage {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "message_id")
     private Long id;
+
     @ManyToOne
     @JoinColumn(name = "chat_id")
     private ChatRoom chatRoom;
+
     @ManyToOne
     @JoinColumn(name = "sender_id")
     private User sender;
+
     @ManyToOne
     @JoinColumn(name = "receiver_id")
     private User receiver;
+
     @Column(name = "chat_timestamp", updatable = false)
     private LocalDateTime timestamp;
+
     private String message;
 
+    @PrePersist
+    protected void onCreate() {
+        this.timestamp = LocalDateTime.now(); // 데이터 생성시 자동으로 현제 시간을 DB에 저장
+    }
 }
