ff1a4532d0d39572113eb56edb46665901ed578f
jngwk <jngwk.ch@gmail.com>
2024-06-04 03:22:24 +0900

    ChatRoomRepo에 대화방에 있는 유저 조회하는 query 메소드 추가

--- back/deukgeun/src/main/java/com/dg/deukgeun/repository/ChatRoomRepository.java
+++ back/deukgeun/src/main/java/com/dg/deukgeun/repository/ChatRoomRepository.java
@@ -1,9 +1,16 @@
 package com.dg.deukgeun.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dg.deukgeun.entity.ChatRoom;
+import com.dg.deukgeun.entity.User;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+
+    // @Query("SELECT cr FROM ChatRoom cr JOIN cr.users u1 JOIN cr.users u2 WHERE
+    // u1.id = :userId1 AND u2.id = :userId2")
+    Optional<ChatRoom> findByUsersContainsAndUsersContains(User user1, User user2);
 
 }
