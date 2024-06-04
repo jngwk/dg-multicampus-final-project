9b27bd571566122a7e62db5b6236227b973b172f
YoungWoo0 <121625793+YoungWoo0@users.noreply.github.com>
2024-06-04 13:21:31 +0900

    사용자 정보 수정을 위한 UpdateUserDTO 파일 추가

--- back/deukgeun/src/main/java/com/dg/deukgeun/controller/UserController.java
+++ back/deukgeun/src/main/java/com/dg/deukgeun/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.dg.deukgeun.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dg.deukgeun.dto.user.LoginDTO;
@@ -32,6 +34,12 @@ public class UserController {
     @PostMapping("/login")
     public ResponseDTO<?> login(@RequestBody LoginDTO requestBody) {
         ResponseDTO<?> result = userService.login(requestBody);
+        return result;
+    }
+
+    @GetMapping("/userInfo")
+    public ResponseDTO<?> getUserInfo(@RequestParam String email) {
+        ResponseDTO<?> result = userService.getUserInfo(email);
         return result;
     }
 }
