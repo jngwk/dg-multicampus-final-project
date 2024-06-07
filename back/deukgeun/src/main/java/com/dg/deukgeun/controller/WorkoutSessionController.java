9f3745b3ab7b763f91b49a3c68c9d0d70e938652
gachudon <doneson7@gmail.com>
2024-06-07 14:29:00 +0900

    chore : 사용하지 않는 확장기능 삭제

--- back/deukgeun/src/main/java/com/dg/deukgeun/controller/WorkoutSessionController.java
+++ back/deukgeun/src/main/java/com/dg/deukgeun/controller/WorkoutSessionController.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters.LocalDateConverter;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
