0c53d541b077915318bc5d0ab2e5ec06d6bab1e6
jngwk <jngwk.ch@gmail.com>
2024-06-10 04:03:54 +0900

    mapping url에 user 추가

--- front/deukgeun/src/api/userInfoApi.js
+++ front/deukgeun/src/api/userInfoApi.js
@@ -1,14 +1,14 @@
 import axios from "axios";
-const prefix = `/api`; // proxy 사용
+const prefix = `/api/user`; // proxy 사용
 
 export const userInfo = async (email) => {
-    try {
-        const response = await axios.get(`${prefix}/userInfo`, {
-            params: { email }
-        });
-        return response.data;
-    } catch (error) {
-        console.error("Error fetching user info:", error);
-        throw error;
-    }
+  try {
+    const response = await axios.get(`${prefix}/userInfo`, {
+      params: { email },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching user info:", error);
+    throw error;
+  }
 };
