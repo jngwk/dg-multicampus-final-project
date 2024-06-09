e7175b7e4c126d5cc3c16c7b0417f1968d6ef6dc
jngwk <jngwk.ch@gmail.com>
2024-06-10 04:06:14 +0900

    회원가입 mapping api 추가

--- front/deukgeun/src/api/signUpApi.js
+++ front/deukgeun/src/api/signUpApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // export const API_SERVER_HOST = "http://localhost:8282";
 // const prefix = `${API_SERVER_HOST}/api`;
-const prefix = `/api`; // proxy 사용
+const prefix = `/api/user`; // proxy 사용
 
 export const signUpGeneral = async (userData) => {
   try {
@@ -24,5 +24,19 @@ export const signUpGym = async (userData) => {
     return response.data;
   } catch (error) {
     throw new Error("Login failed");
+  }
+};
+
+export const sendVerificationEmail = async (email, code) => {
+  try {
+    const response = await axios.post(`${prefix}/sendCode`, {
+      email: email,
+      code: code,
+    });
+    console.log("response: ", response);
+    console.log("Email sent successfully");
+    return response;
+  } catch (error) {
+    console.error("Error sending email", error);
   }
 };
