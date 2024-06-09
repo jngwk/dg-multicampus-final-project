2145663b88436fd4a9e73e0ca7df7b7234edfd0b
jngwk <jngwk.ch@gmail.com>
2024-06-10 04:06:24 +0900

    이메일 인증 관련 유효성 검사 추가

--- front/deukgeun/src/hooks/useValidation.jsx
+++ front/deukgeun/src/hooks/useValidation.jsx
@@ -9,21 +9,24 @@ const useValidation = () => {
   }, [errors]);
 
   const validateUserName = (userName, tempErrors) => {
-    console.log("Validating userName: ", userName); // Debugging
+    // console.log("Validating userName: ", userName); // Debugging
     if (!/^[가-힣]+$/.test(userName)) {
       tempErrors.userName = "이름은 한글로만 입력해주세요";
     }
   };
 
   const validateEmail = (email, tempErrors) => {
-    console.log("Validating email: ", email); // Debugging
+    // console.log("Validating email: ", email); // Debugging
     if (!/\S+@\S+\.\S+/.test(email)) {
       tempErrors.email = "유효한 이메일 주소를 입력해주세요";
     }
   };
+  // const validateEmailVerification = (verified, tempErrors) => {
+  //   !verified && (tempErrors.email = "이메일 인증이 되지 않았습니다.");
+  // };
 
   const validatePassword = (password, tempErrors) => {
-    console.log("Validating password: ", password); // Debugging
+    // console.log("Validating password: ", password); // Debugging
     if (
       !/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/.test(
         password
@@ -35,20 +38,20 @@ const useValidation = () => {
   };
 
   const validateConfirmPassword = (password, confirmPassword, tempErrors) => {
-    console.log("Validating confirmPassword: ", confirmPassword); // Debugging
+    // console.log("Validating confirmPassword: ", confirmPassword); // Debugging
     if (password !== confirmPassword) {
       tempErrors.confirmPassword = "비밀번호가 일치하지 않습니다";
     }
   };
 
   const validateNotEmpty = (field, value, tempErrors) => {
-    console.log(`Validating ${field}: `, value); // Debugging
+    // console.log(`Validating ${field}: `, value); // Debugging
     if (!value) {
       tempErrors[field] = "필수 입력 항목입니다";
     }
   };
 
-  const validateInput = (field, value, password = "") => {
+  const validateInput = (field, value, password = "", verified = true) => {
     setErrors((prevErrors) => {
       const { [field]: removedError, ...restErrors } = prevErrors;
       return restErrors;
@@ -62,12 +65,13 @@ const useValidation = () => {
         break;
       case "email":
         validateEmail(value, tempErrors);
+        !verified && (tempErrors.email = "이메일 인증이 완료되지 않았습니다.");
         break;
       case "password":
         validatePassword(value, tempErrors);
         break;
       case "confirmPassword":
-        console.log(password, field, value);
+        // console.log(password, field, value);
         validateConfirmPassword(password, value, tempErrors);
         break;
       default:
@@ -80,10 +84,11 @@ const useValidation = () => {
     }));
   };
 
-  const validateForm = (userData, confirmPassword) => {
+  const validateForm = (userData, confirmPassword, verified = true) => {
     const tempErrors = {};
     validateUserName(userData.userName, tempErrors);
     validateEmail(userData.email, tempErrors);
+    !verified && (tempErrors.email = "이메일 인증이 완료되지 않았습니다.");
     validatePassword(userData.password, tempErrors);
     validateConfirmPassword(userData.password, confirmPassword, tempErrors);
     Object.entries(userData).forEach(([key, value]) => {
@@ -91,7 +96,6 @@ const useValidation = () => {
       validateNotEmpty(key, value, tempErrors);
     });
     validateNotEmpty("confirmPassword", confirmPassword, tempErrors);
-
     setErrors(tempErrors);
     console.log("유효성 검사 후 에러: ", tempErrors); // Debugging
     return Object.keys(tempErrors).length === 0;
@@ -100,13 +104,11 @@ const useValidation = () => {
   const resetErrors = () => {
     setErrors({});
   };
+
   return {
     errors,
+    setErrors,
     resetErrors,
-    validateUserName,
-    validateEmail,
-    validatePassword,
-    validateConfirmPassword,
     validateNotEmpty,
     validateInput,
     validateForm,
