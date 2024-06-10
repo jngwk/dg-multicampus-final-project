d152092efaaf8932c7fdd50bf43664bf18b97068
HeeJeong Seo <123150923+ggomiyyomi@users.noreply.github.com>
2024-06-10 16:46:59 +0900

    회원관리 테이블 생성/임시 더미데이터

--- front/deukgeun/src/pages/ChatRoom.jsx
+++ front/deukgeun/src/pages/ChatRoom.jsx
@@ -11,22 +11,22 @@ export default function ChatRoom() {
     return (
         <Layout>
         <div className="absolute top-[15%] left-[7%] w-5/6 py-5">
-        <div className="flex items-center w-[920px] px-10 pb-2 ">
-          <IoChatbubbles color="#ffbe98" size="56"/> 
-          <span className="font-semibold text-2xl mx-3 "> 득-근 CHAT </span>
-          <button><IoAddCircle color='#E6E6E6' size="28"/></button>
-        </div>
-        
-        <div className="ChatContainer flex justify-center px-2 my-2">
-          {/* columns주고 여러개 */}
-          <div className="w-2/6 h-[35rem] mx-8 border-none rounded-lg bg-peach-fuzz bg-opacity-20">
-            <div className="flex-col space-y-4 overflow-y-scroll overflow-x-hidden h-[35rem] w-full pl-4 py-2 scrollbar-hide md:scrollbar-default">
-            <Chatlist/>
-            </div>
+          <div className="flex items-center w-[920px] px-10 pb-2 ">
+            <IoChatbubbles color="#ffbe98" size="56"/> 
+            <span className="font-semibold text-2xl mx-3 "> 득-근 CHAT </span>
+            <button><IoAddCircle color='#E6E6E6' size="28"/></button>
           </div>
-            <Chatting/>
-            <ChatUserInfo/>
-        </div>
+          
+          <div className="ChatContainer flex justify-center px-2 my-2">
+            {/* columns주고 여러개 */}
+            <div className="w-2/6 h-[35rem] mx-8 border-none rounded-lg bg-peach-fuzz bg-opacity-20">
+              <div className="flex-col space-y-4 overflow-y-scroll overflow-x-hidden h-[35rem] w-full pl-4 py-2 scrollbar-hide md:scrollbar-default">
+              <Chatlist/>
+              </div>
+            </div>
+              <Chatting/>
+              <ChatUserInfo/>
+          </div>
         </div>
       </Layout>
       
