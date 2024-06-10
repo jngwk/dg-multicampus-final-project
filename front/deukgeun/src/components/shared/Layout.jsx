2920f8f26b1d08e1febba8a99b335789733653cc
jngwk <jngwk.ch@gmail.com>
2024-06-10 16:48:08 +0900

    문의하기 form 추가 완료

--- front/deukgeun/src/components/shared/Layout.jsx
+++ front/deukgeun/src/components/shared/Layout.jsx
@@ -3,10 +3,10 @@ import Header from "./Header";
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <div>
       <Header />
-      <div className="relative min-h-[90dvh]">{children}</div>
-    </>
+      <div className="relative min-h-[90dvh] w-5/6 mx-auto">{children}</div>
+    </div>
   );
 };
 
