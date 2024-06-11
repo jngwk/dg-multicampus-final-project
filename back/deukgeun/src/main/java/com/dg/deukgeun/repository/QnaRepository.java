b9e0f68f350e81d7f72e61cfc5d9d3fc80bf2803
mingu00 <tlaalsrb1323@naver.com>
2024-06-11 22:06:16 +0900

    문의하기 back 코드 구현 완료

---
+++ back/deukgeun/src/main/java/com/dg/deukgeun/repository/QnaRepository.java
@@ -0,0 +1,9 @@
+package com.dg.deukgeun.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dg.deukgeun.entity.Qna;
+
+public interface QnaRepository extends JpaRepository<Qna, Integer>{
+
+}
