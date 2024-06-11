e571da0c8d5bf2667966947058e7128954a66a75
jngwk <jngwk.ch@gmail.com>
2024-06-11 22:27:20 +0900

    폰트 적용 안되는 오류 수정

--- front/deukgeun/tailwind.config.js
+++ front/deukgeun/tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
       ...colors,
     },
     fontFamily: {
-      sans: ["Noto Sans Kr"],
+      sans: ['"Noto Sans Kr"', "sans-serif"],
     },
     extend: {
       keyframes: {
