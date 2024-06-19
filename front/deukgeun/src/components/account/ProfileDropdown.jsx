f9e3834735f38145cfb7d05f09961542549299c5
jngwk <jngwk.ch@gmail.com>
2024-06-20 05:12:29 +0900

    아이콘 추가

--- front/deukgeun/src/components/account/ProfileDropdown.jsx
+++ front/deukgeun/src/components/account/ProfileDropdown.jsx
@@ -16,7 +16,7 @@ const ProfileDropdown = ({ type }) => {
   };
 
   return (
-    <div className="relative border border-gray-400 w-64 h-fit rounded-lg p-3 bg-white shadow-sm z-40">
+    <div className="relative border border-gray-400 w-52 h-fit rounded-lg p-3 bg-white shadow-sm z-40">
       <div className="border-b-[0.5px] border-gray-400 p-2">
         {/* 사용자 이름 표시 */}
         {loading ? (
@@ -31,8 +31,8 @@ const ProfileDropdown = ({ type }) => {
       <div>
         <ul className="mt-4">
           <li className="profile-dropdown-list" onClick={toggleModal}>
-            <img className="inline-block peer w-7 mx-3" src="" alt="icon" />
-            <span>내 정보</span>
+            <box-icon name="id-card" color="#ffbe98" size="sm"></box-icon>
+            <span className="ml-3">내 정보</span>
           </li>
           {isModalVisible ? (
             <UserInfo toggleModal={toggleModal} userData={userData} />
@@ -41,25 +41,25 @@ const ProfileDropdown = ({ type }) => {
           )}
           <li
             className="profile-dropdown-list"
-            onClick={() => customNavigate("/Calendar")}
+            onClick={() => customNavigate("/calendar")}
           >
-            <img className="inline-block peer w-7 mx-3" src="" alt="icon" />
-            <span>운동일지</span>
+            <box-icon name="calendar" color="#ffbe98" size="sm"></box-icon>
+            <span className="ml-3">운동일지</span>
           </li>
           <li
             className="profile-dropdown-list"
-            onClick={() => customNavigate("/Chat")}
+            onClick={() => customNavigate("/chat")}
           >
-            <img className="inline-block peer w-7 mx-3" src="" alt="icon" />
-            <span>대화방</span>
+            <box-icon name="conversation" color="#ffbe98" size="sm"></box-icon>
+            <span className="ml-3">대화방</span>
           </li>
           <li className="profile-dropdown-list">
-            <img className="inline-block peer w-7 mx-3" src="" alt="icon" />
-            <span>문의내역</span>
+            <box-icon name="question-mark" color="#ffbe98" size="sm"></box-icon>
+            <span className="ml-3">문의내역</span>
           </li>
           <li className="profile-dropdown-list" onClick={handleLogout}>
-            <img className="inline-block peer w-7 mx-3" src="" alt="icon" />
-            <span>로그아웃</span>
+            <box-icon name="log-out" color="#ffbe98" size="sm"></box-icon>
+            <span className="ml-3">로그아웃</span>
           </li>
         </ul>
       </div>
