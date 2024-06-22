d7c317656182b18a2be428ac0c5af1ec810d8b8d
YoungWoo0 <121625793+YoungWoo0@users.noreply.github.com>
2024-06-23 02:25:38 +0900

    review 작성, 수정, 삭제 조회 기능(gymId 연결 필요 현재 하드 코딩으로 넣어놓음) front/back 연결

--- front/deukgeun/src/components/modals/ReviewEditModal.jsx
+++ front/deukgeun/src/components/modals/ReviewEditModal.jsx
@@ -2,25 +2,24 @@ import React, { useState, useEffect } from "react";
 import { useAuth } from "../../context/AuthContext";
 import ModalLayout from "./ModalLayout";
 import TextArea from "../shared/TextArea";
-import { IoMdPhotos } from "react-icons/io";
 import Button from "../shared/Button";
-import { addReview } from "../../api/reviewApi";
+import { updateReview } from "../../api/reviewApi";
 import Fallback from "../shared/Fallback";
 
-const ReviewModal = ({ toggleModal, gymId }) => {
-    
+const ReviewEditModal = ({ toggleModal, gymId, review, onUpdateReview }) => {
     const { userData, loading } = useAuth();
     const [formValues, setFormValues] = useState({
         gymId: parseInt(gymId, 10),
-        comment: "",
-        rating: 0,
-        userId: 0,
-        userName: "",
-        email: "",
-        // reviewImg: "",
+        comment: review.comment,
+        rating: review.rating,
+        userId: review.userId,
+        userName: review.userName,
+        email: review.email,
+        id: review.id,
     });
+
     useEffect(() => {
-        if(userData){
+        if (userData) {
             setFormValues((prevValues) => ({
                 ...prevValues,
                 userId: userData.userId,
@@ -28,25 +27,15 @@ const ReviewModal = ({ toggleModal, gymId }) => {
                 email: userData.email,
             }));
         }
-        
     }, [userData]);
+
     const handleInputChange = (e) => {
         const { name, value } = e.target;
         setFormValues({
             ...formValues,
             [name]: value,
         });
-        console.log(`Input changed: ${name} = ${value}`);
     };
-
-    // const handleImageChange = (e) => {
-    //   const file = e.target.files[0];
-    //   setFormValues({
-    //     ...formValues,
-    //     reviewImg: file,
-    //   });
-    //   console.log(`Image selected: ${file.name}`);
-    // };
 
     const handleRatingChange = (e) => {
         const { value } = e.target;
@@ -54,7 +43,6 @@ const ReviewModal = ({ toggleModal, gymId }) => {
             ...formValues,
             rating: parseInt(value, 10),
         });
-        console.log(`Rating changed: ${value}`);
     };
 
     const handleSubmit = async (e) => {
@@ -65,32 +53,26 @@ const ReviewModal = ({ toggleModal, gymId }) => {
                 comment: formValues.comment,
                 rating: formValues.rating,
                 regDate: new Date().toISOString().split('T')[0],
-            }
-            console.log("Form data being submitted:", {
-                gymId: formValues.gymId,
-                comment: formValues.comment,
-                rating: formValues.rating,
-                userId: userData.userId,
-                userName: userData.userName,
-                email: userData.email,
-            });
-            const res = await addReview(formData);
-            console.log("Response: ", res);
-            // toggleModal();
+            };
+            const res = await updateReview(formData);
+            onUpdateReview(formData);
+            toggleModal();
         } catch (error) {
-            console.error("Failed to add review", error);
+            console.error("Failed to update review", error);
         }
     };
+
     if (loading) {
         return <Fallback />;
     }
+
     return (
         <ModalLayout toggleModal={toggleModal}>
             <form onSubmit={handleSubmit}>
                 <div className="flex flex-col h-96">
                     <div className="flex flex-col gap-1 justify-center items-center">
                         <div className="mb-2 font-semibold text-xl">
-                            리뷰작성
+                            리뷰 수정
                             <div className="mt-2 w-16 border-b-2 border-grayish-red border-opacity-20"></div>
                         </div>
                         <label htmlFor="commentInput">후기를 작성해주세요.
@@ -120,18 +102,8 @@ const ReviewModal = ({ toggleModal, gymId }) => {
                             ))}
                         </div>
                     </div>
-
                     <div>
-                        {/* <label className="flex text-sm items-center cursor-pointer">
-                            <IoMdPhotos className="w-7 h-7 pr-1" />
-                            파일 이미지 넣기
-                            <input
-                                type="file"
-                                className="hidden"
-                                onChange={handleImageChange}
-                            />
-                        </label> */}
-                        <Button label="작성" width="100px" className={`float-right mt-2`} type="submit" />
+                        <Button label="수정" width="100px" className={`float-right mt-2`} type="submit" />
                     </div>
                 </div>
             </form>
@@ -139,4 +111,4 @@ const ReviewModal = ({ toggleModal, gymId }) => {
     );
 };
 
-export default ReviewModal;
+export default ReviewEditModal;
