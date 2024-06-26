aa05f4c1cee1f1e45289b8c9e3e8307a6e537ff1
jngwk <jngwk.ch@gmail.com>
2024-06-27 01:31:12 +0900

    전역에서 로그인 모달 사용 가능하게 설정

--- front/deukgeun/src/components/shared/Layout.jsx
+++ front/deukgeun/src/components/shared/Layout.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Header from "./Header";
+import { useLoginModalContext } from "../../context/LoginModalContext";
+import LoginModal from "../../components/modals/LoginModal";
 
 const Layout = ({ children }) => {
+  const { toggleLoginModal, isLoginModalVisible } = useLoginModalContext();
+
   return (
     <div>
       <Header />
       <div className="justify-center min-h-[90dvh] w-full">{children}</div>
+      {isLoginModalVisible && <LoginModal toggleModal={toggleLoginModal} />}
     </div>
   );
 };
