3d9314ceda6321b7fc0017e2f301f1db049824a4
jngwk <jngwk.ch@gmail.com>
2024-07-04 01:04:26 +0900

    가격 쿼리문 수정

--- back/deukgeun/src/main/java/com/dg/deukgeun/repository/GymRepository.java
+++ back/deukgeun/src/main/java/com/dg/deukgeun/repository/GymRepository.java
@@ -13,41 +13,42 @@ import com.dg.deukgeun.entity.Gym;
 import com.dg.deukgeun.entity.User;
 
 public interface GymRepository extends JpaRepository<Gym, Integer> {
-    Optional<Gym> findByUser(User user);
+        Optional<Gym> findByUser(User user);
 
-    @Query("SELECT g FROM Gym g JOIN g.user u WHERE "
-            + "REPLACE(LOWER(u.userName), ' ', '') LIKE LOWER(CONCAT('%', :searchWord, '%')) "
-            + "OR REPLACE(LOWER(g.gymName), ' ', '') LIKE LOWER(CONCAT('%', :searchWord, '%')) "
-            + "OR REPLACE(LOWER(g.address), ' ', '') LIKE LOWER(CONCAT('%', :searchWord, '%'))")
-    List<Gym> searchGyms(@Param("searchWord") String searchWord);
+        @Query("SELECT g FROM Gym g JOIN g.user u WHERE "
+                        + "REPLACE(LOWER(u.userName), ' ', '') LIKE LOWER(CONCAT('%', :searchWord, '%')) "
+                        + "OR REPLACE(LOWER(g.gymName), ' ', '') LIKE LOWER(CONCAT('%', :searchWord, '%')) "
+                        + "OR REPLACE(LOWER(g.address), ' ', '') LIKE LOWER(CONCAT('%', :searchWord, '%'))")
+        List<Gym> searchGyms(@Param("searchWord") String searchWord);
 
-    @Query("SELECT g FROM Gym g JOIN g.user u WHERE "
-            + "g.operatingHours LIKE CONCAT('%', '24시간', '%') AND ("
-            + "REPLACE(LOWER(u.userName), ' ', '') LIKE LOWER(CONCAT('%', :searchWord, '%')) "
-            + "OR REPLACE(LOWER(g.gymName), ' ', '') LIKE LOWER(CONCAT('%', :searchWord, '%')) "
-            + "OR REPLACE(LOWER(g.address), ' ', '') LIKE LOWER(CONCAT('%', :searchWord, '%')))")
-    List<Gym> searchGymsByOperatingHours(@Param("searchWord") String searchWord);
+        @Query("SELECT g FROM Gym g JOIN g.user u WHERE "
+                        + "g.operatingHours LIKE CONCAT('%', '24시간', '%') AND ("
+                        + "REPLACE(LOWER(u.userName), ' ', '') LIKE LOWER(CONCAT('%', :searchWord, '%')) "
+                        + "OR REPLACE(LOWER(g.gymName), ' ', '') LIKE LOWER(CONCAT('%', :searchWord, '%')) "
+                        + "OR REPLACE(LOWER(g.address), ' ', '') LIKE LOWER(CONCAT('%', :searchWord, '%')))")
+        List<Gym> searchGymsByOperatingHours(@Param("searchWord") String searchWord);
 
-    @Query("SELECT g FROM Gym g JOIN g.user u WHERE "
-            + "g.address LIKE CONCAT('%', :location, '%') AND ("
-            + "REPLACE(LOWER(u.userName), ' ', '') LIKE LOWER(CONCAT('%', :searchWord, '%')) "
-            + "OR REPLACE(LOWER(g.gymName), ' ', '') LIKE LOWER(CONCAT('%', :searchWord, '%')) "
-            + "OR REPLACE(LOWER(g.address), ' ', '') LIKE LOWER(CONCAT('%', :searchWord, '%')))")
-    List<Gym> searchGymsByLocation(@Param("searchWord") String searchWord, @Param("location") String location);
+        @Query("SELECT g FROM Gym g JOIN g.user u WHERE "
+                        + "g.address LIKE CONCAT('%', :location, '%') AND ("
+                        + "REPLACE(LOWER(u.userName), ' ', '') LIKE LOWER(CONCAT('%', :searchWord, '%')) "
+                        + "OR REPLACE(LOWER(g.gymName), ' ', '') LIKE LOWER(CONCAT('%', :searchWord, '%')) "
+                        + "OR REPLACE(LOWER(g.address), ' ', '') LIKE LOWER(CONCAT('%', :searchWord, '%')))")
+        List<Gym> searchGymsByLocation(@Param("searchWord") String searchWord, @Param("location") String location);
 
-    @Query("SELECT g FROM Gym g JOIN g.user u JOIN g.products p "
-            + "WHERE p.productName LIKE '%1개월%' "
-            + "AND ("
-            + "REPLACE(LOWER(u.userName), ' ', '') LIKE LOWER(CONCAT('%', :searchWord, '%')) "
-            + "OR REPLACE(LOWER(g.gymName), ' ', '') LIKE LOWER(CONCAT('%', :searchWord, '%')) "
-            + "OR REPLACE(LOWER(g.address), ' ', '') LIKE LOWER(CONCAT('%', :searchWord, '%'))"
-            + ") "
-            + "GROUP BY g "
-            + "HAVING AVG(p.price) IS NOT NULL "
-            + "ORDER BY AVG(p.price) ASC")
-    Page<Gym> searchGymsByPrice(@Param("searchWord") String searchWord, Pageable pageable);
+        @Query("SELECT g FROM Gym g JOIN g.user u JOIN g.products p "
+                        + "WHERE p.productName LIKE '%1개월%' "
+                        + "AND p.productName NOT LIKE '%PT%' "
+                        + "AND ("
+                        + "REPLACE(LOWER(u.userName), ' ', '') LIKE LOWER(CONCAT('%', :searchWord, '%')) "
+                        + "OR REPLACE(LOWER(g.gymName), ' ', '') LIKE LOWER(CONCAT('%', :searchWord, '%')) "
+                        + "OR REPLACE(LOWER(g.address), ' ', '') LIKE LOWER(CONCAT('%', :searchWord, '%'))"
+                        + ") "
+                        + "GROUP BY g "
+                        + "HAVING AVG(p.price) IS NOT NULL AND AVG(p.price) <= ALL (SELECT AVG(p2.price) FROM Product p2 WHERE p2.productName LIKE '%1개월%' AND p2.productName NOT LIKE '%PT%') "
+                        + "ORDER BY AVG(p.price) ASC")
+        Page<Gym> searchGymsByPrice(@Param("searchWord") String searchWord, Pageable pageable);
 
-    public Optional<Gym> findByUser_UserId(Integer userId);
+        public Optional<Gym> findByUser_UserId(Integer userId);
 
-    Optional<Gym> findByCrNumber(String crNumber);
+        Optional<Gym> findByCrNumber(String crNumber);
 }
