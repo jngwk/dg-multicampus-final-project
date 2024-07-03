03716e80d068f88d53f3da09cd2bb36d7f9f449a
mingu00 <tlaalsrb1323@naver.com>
2024-07-03 13:42:06 +0900

    더미데이터 추가

--- front/deukgeun/src/api/ptSessionApi.js
+++ front/deukgeun/src/api/ptSessionApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // export const API_SERVER_HOST = "http://localhost:8282";
-// const prefix = `${API_SERVER_HOST}/api/ptSession`;
+// const prefix = `${API_SERVER_HOST}/api/personalTraining`;
 const prefix = `/api/personalTraining`; // proxy 사용
 
 export const registerPTSession = async (workoutSessionData) => {
