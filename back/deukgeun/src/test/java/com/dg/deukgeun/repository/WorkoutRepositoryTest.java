f7efbdc87e2ae1010e786bbd874079e060456fc3
jngwk <jngwk.ch@gmail.com>
2024-07-06 00:17:46 +0900

    테스트 주석

--- back/deukgeun/src/test/java/com/dg/deukgeun/repository/WorkoutRepositoryTest.java
+++ back/deukgeun/src/test/java/com/dg/deukgeun/repository/WorkoutRepositoryTest.java
@@ -1,28 +1,29 @@
-package com.dg.deukgeun.repository;
+// package com.dg.deukgeun.repository;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-import com.dg.deukgeun.entity.Workout;
+// import com.dg.deukgeun.entity.Workout;
 
-import lombok.extern.log4j.Log4j2;
+// import lombok.extern.log4j.Log4j2;
 
-@SpringBootTest
-@Log4j2
-public class WorkoutRepositoryTest {
-    @Autowired
-    private WorkoutRepository workoutRepository;
+// @SpringBootTest
+// @Log4j2
+// public class WorkoutRepositoryTest {
+// @Autowired
+// private WorkoutRepository workoutRepository;
 
-    @Test
-    public void repositoryTest(){
-        log.info("--------------------");
-        log.info(workoutRepository);
-    }
+// @Test
+// public void repositoryTest(){
+// log.info("--------------------");
+// log.info(workoutRepository);
+// }
 
-    @Test
-    public void testInsert(){
-        Workout workout = Workout.builder().workoutSessionId(null).workoutName("삼두").build();
-        workoutRepository.save(workout);
-    }
-}
+// @Test
+// public void testInsert(){
+// Workout workout =
+// Workout.builder().workoutSessionId(null).workoutName("삼두").build();
+// workoutRepository.save(workout);
+// }
+// }
