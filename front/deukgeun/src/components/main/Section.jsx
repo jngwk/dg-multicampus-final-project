e6e2d70fbef248979c39c04bb533a7d7aa8daea5
jngwk <jngwk.ch@gmail.com>
2024-07-06 18:50:49 +0900

    메인 수정

--- front/deukgeun/src/components/main/Section.jsx
+++ front/deukgeun/src/components/main/Section.jsx
@@ -309,7 +309,7 @@ const Section = ({}) => {
 
       {/* section 4 */}
       <div
-        className={`snap-start h-[100dvh] w-full flex flex-col gap-60 justify-center items-center text-xl`}
+        className={`snap-start h-[100dvh] w-full flex flex-col justify-evenly items-center text-xl`}
       >
         <div className="flex justify-center items-center gap-4 relative">
           <span className="text-5xl">💪</span>
@@ -380,7 +380,7 @@ const Section = ({}) => {
           <div className="border bg-gray-300 rounded-full w-[600px] h-[400px]"></div>
           <div className="border bg-gray-300 rounded-full w-[510px] h-[340px]"></div>
         </div> */}
-        <div className="relative flex gap-6 justify-center items-center">
+        <div className="relative flex gap-4 justify-center items-center">
           <div>
             <span className="font-bold text-3xl">사장님도 '득근'하세요!</span>
             <div className="absolute left-[136px]">
