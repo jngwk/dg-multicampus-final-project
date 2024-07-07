1f66a7f2bb142df308da27112e884a7cf5dab29a
jngwk <jngwk.ch@gmail.com>
2024-07-08 08:27:25 +0900

    z index 수정

--- front/deukgeun/src/components/shared/Fallback.jsx
+++ front/deukgeun/src/components/shared/Fallback.jsx
@@ -3,7 +3,7 @@ import SyncLoader from "react-spinners/SyncLoader";
 
 const Fallback = () => {
   return (
-    <div className="absolute h-full w-full top-0 left-0 bg-white flex flex-col gap-8 justify-center items-center">
+    <div className="absolute h-full w-full top-0 left-0 bg-white flex flex-col gap-8 justify-center items-center z-50">
       <SyncLoader color="#ffbe98" margin={15} size={40} />
       <span className="text-3xl translate-x-3 font-extrabold text-grayish-red animate-fadeInOut">
         Loading...
