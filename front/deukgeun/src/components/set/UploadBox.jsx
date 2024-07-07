ffb2c0c3697b4c1bef15b75421f953f4a70d8c91
HeeJeong Seo <123150923+ggomiyyomi@users.noreply.github.com>
2024-07-07 14:11:26 +0900

    헬스권피티권&헬스장정보&리뷰 디자인 수정

--- front/deukgeun/src/components/set/UploadBox.jsx
+++ front/deukgeun/src/components/set/UploadBox.jsx
@@ -36,31 +36,34 @@ const UploadBox = ({name, required, onChange }) => {
   };
 
   return (
-    <div className="w-full pb-3 overflow-y-hidden overflow-x-auto flex flex-nowrap gap-4 scrollbar">
+    <div className="w-full pb-3 overflow-y-hidden overflow-x-auto flex flex-col flex-nowrap gap-4 scrollbar">
+    
       <div className="flex-shrink-0 w-fit h-fit">
         <label className="flex flex-col justify-center items-center w-44 h-36 bg-white rounded-md border border-gray-400 border-dashed p-4 overflow-hidden cursor-pointer hover:border-bright-orange">
           <input type="file" name={name} required={required} accept="images/*" className="hidden" onChange={handleFileChange} multiple />
           <Logo />
-          <p className="font-medium text-sm my-3 pre-line">
-            클릭 혹은 파일을 <br />
-            이곳에 드롭하세요.
+          <p className="font-medium text-center text-sm my-3 pre-line">
+            클릭하여 파일을 <br />
+            이곳에 가져오세요.
           </p>
           <p className="m-0 text-xs">파일당 최대 3MB</p>
         </label>
       </div>
-      {previewSrcList.map((src, index) => (
-        <div key={index} className="flex-shrink-0 w-fit h-fit relative">
-          <div className="flex flex-col justify-center items-center w-44 h-36 bg-white rounded-md border border-gray-400 p-2 overflow-hidden">
-            <img src={src} alt="미리보기" className="w-24 h-24 object-cover" />
-            <button
-              className="w-4 h-4 flex justify-center items-center absolute top-1 right-1 rounded-full text-red-300 hover:bg-red-300 hover:text-white transition duration-300"
-              onClick={() => handleRemovePreview(index)}
-            >
-              <PiXCircle/>
-            </button>
+      <div className="grid grid-cols-4 gap-4">
+        {previewSrcList.map((src, index) => (
+          <div key={index} className="relative w-fit h-fit">
+            <div className="flex flex-col justify-center items-center">
+              <img src={src} alt="미리보기" className="w-24 h-24 object-cover rounded-lg border" />
+              <button
+                className="w-6 h-6 flex justify-center items-center absolute top-1 right-1 rounded-full text-red-300 hover:bg-red-300 hover:text-white transition duration-300"
+                onClick={() => handleRemovePreview(index)}
+              >
+                <PiXCircle />
+              </button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
