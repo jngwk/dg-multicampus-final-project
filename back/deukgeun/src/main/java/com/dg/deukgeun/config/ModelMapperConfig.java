13e771896f7d2c1929b4182754749c160eed1078
jngwk <jngwk.ch@gmail.com>
2024-07-08 12:02:01 +0900

    모델 매퍼 오류 수정

--- back/deukgeun/src/main/java/com/dg/deukgeun/config/ModelMapperConfig.java
+++ back/deukgeun/src/main/java/com/dg/deukgeun/config/ModelMapperConfig.java
@@ -1,25 +1,26 @@
-package com.dg.deukgeun.config;
+// package com.dg.deukgeun.config;
 
-import org.modelmapper.ModelMapper;
-import org.modelmapper.PropertyMap;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import org.modelmapper.ModelMapper;
+// import org.modelmapper.PropertyMap;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-import com.dg.deukgeun.dto.WorkoutSessionDTO;
-import com.dg.deukgeun.entity.WorkoutSession;
+// import com.dg.deukgeun.dto.WorkoutSessionDTO;
+// import com.dg.deukgeun.entity.WorkoutSession;
 
-@Configuration
-public class ModelMapperConfig {
+// @Configuration
+// public class ModelMapperConfig {
 
-    @Bean
-    public ModelMapper modelMapper() {
-        ModelMapper modelMapper = new ModelMapper();
-        modelMapper.addMappings(new PropertyMap<WorkoutSessionDTO, WorkoutSession>() {
-            @Override
-            protected void configure() {
-                map().setUser(source.getPtSession().getPt().getUser());
-            }
-        });
-        return modelMapper;
-    }
-}
+// @Bean
+// public ModelMapper modelMapper() {
+// ModelMapper modelMapper = new ModelMapper();
+// modelMapper.addMappings(new PropertyMap<WorkoutSessionDTO, WorkoutSession>()
+// {
+// @Override
+// protected void configure() {
+// map().setUser(source.getPtSession().getPt().getUser());
+// }
+// });
+// return modelMapper;
+// }
+// }
